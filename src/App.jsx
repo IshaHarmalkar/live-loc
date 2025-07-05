1ab033ddfc1357aaf892244466b5ed95d9589cea
IshaHarmalkar <167905999+IshaHarmalkar@users.noreply.github.com>
2025-07-05 19:37:42 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import FirebaseLocationSharing from "./components/FirebaseLocationSharing";
+
+function App() {
+  return <FirebaseLocationSharing />;
+}
+
+export default App;
